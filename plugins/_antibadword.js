e96f7058b8d8a2a120f41e90dec6137c4a256829
ShooC <118227993+ShooC@users.noreply.github.com>
2022-12-10 05:01:29 +0700

    Update _antibadword.js

--- plugins/_antibadword.js
+++ plugins/_antibadword.js
@@ -1,4 +1,4 @@
-let { GroupSettingChange } = require('@adiwajshing/baileys')
+/*let { GroupSettingChange } = require('@adiwajshing/baileys')
 let handler = m => m
 
 let badwordRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole/i // tambahin sendiri
@@ -29,4 +29,4 @@ ketik *#astagfirullah* atau *#maaf* untuk mengurangi warning
     }
     return !0
 }
-module.exports = handler
+module.exports = handler*/
